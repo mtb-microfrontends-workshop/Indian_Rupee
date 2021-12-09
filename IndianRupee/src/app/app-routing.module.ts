776983f35df8fd8a3903aa131d0bb5f9d870aeee
kvahldieck <kvahldieck@mtb.com>
2021-12-09 15:45:38 -0500

    IndianRupee application loads in browser

--- IndianRupee/src/app/app-routing.module.ts
+++ IndianRupee/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component1Component } from './component1/component1.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
+
 const routes: Routes = [
-  { path: '**', component: EmptyRouteComponent }
+  { path: '**', component: EmptyRouteComponent },
+  { path: 'component1', component: Component1Component }
+
 ];
 
 @NgModule({
